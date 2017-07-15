9a64f130d23bd3099dc2c18c5c89574c8ddb3a58
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2017-07-15 23:37:44 +0300

    Initial scr folder structure and default data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers';
+import App from './components/App';
+
+import './style.sass';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+
+const r = combineReducers({
+});
+
+export default r;
